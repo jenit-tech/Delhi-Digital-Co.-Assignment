32d2725d502a113d07ad44a68314a36d54c7ce82
Jenit Joseph Jose <147470538+jenit-tech@users.noreply.github.com>
2025-01-13 22:18:23 +0530

    Update Login.jsx

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
     
 
     try {
-      const result = await axios.post("https://delhi-digital-co-assignment-backend.onrender.com", data);
+      const result = await axios.post("https://delhi-digital-co-assignment-backend.onrender.com/invoice/login", data);
 
       if (result.status === 201) {
         console.log("Login successful");
